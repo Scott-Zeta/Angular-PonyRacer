7bcb904c1dd48bd6cdb20663380b7498747d892a
Scott Zeta <scott-zeta@outlook.com>
2024-10-16 13:28:22 +1030

    set up test

--- src/app/races/races.component.spec.ts
+++ src/app/races/races.component.spec.ts
@@ -1,23 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { RacesComponent } from './races.component';
 
 describe('RacesComponent', () => {
-  let component: RacesComponent;
-  let fixture: ComponentFixture<RacesComponent>;
+  beforeEach(() => TestBed.configureTestingModule({}));
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RacesComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(RacesComponent);
-    component = fixture.componentInstance;
+  it('should display every race', () => {
+    const fixture = TestBed.createComponent(RacesComponent);
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    const element = fixture.nativeElement as HTMLElement;
+    const raceNames = element.querySelectorAll('h2');
+    expect(raceNames.length).withContext('You should have an `h2` element per race in your template').toBe(2);
+    expect(raceNames[0].textContent).toContain('Lyon');
+    expect(raceNames[1].textContent).toContain('London');
   });
 });
