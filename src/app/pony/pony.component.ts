b9fd84746d3b3a17a31c9d833c44540979861a57
Scott Zeta <scott-zeta@outlook.com>
2024-10-18 15:12:54 +1030

    Refactor pony component to add click event and emit pony model

--- src/app/pony/pony.component.ts
+++ src/app/pony/pony.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PonyModel } from '../models/pony.model';
 
 @Component({
@@ -10,8 +10,13 @@ import { PonyModel } from '../models/pony.model';
 })
 export class PonyComponent {
   @Input({ required: true }) ponyModel!: PonyModel;
+  @Output() ponyClicked = new EventEmitter<PonyModel>();
 
   getPonyImageUrl(): string {
     return `assets/images/pony-${this.ponyModel.color.toLowerCase()}.gif`;
+  }
+
+  clicked(): void {
+    this.ponyClicked.emit(this.ponyModel);
   }
 }
